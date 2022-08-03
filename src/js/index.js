2ef9ccfdbe5ff03a2a24c480635d49b513e4ac0a
Luis Salcedo <luis.salcedo0906@gmail.com>
2022-08-03 13:35:48 -0500

    loading song list

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,39 @@
-document.addEventListener('DOMContentLoaded', function(){
-    const playList = document.querySelector('.play-list');
+document.addEventListener('DOMContentLoaded', async function(){
+    const jsmediatags = window.jsmediatags;
+    const playListContainer = document.querySelector('.play-list-container');
+    const playList = document.querySelector('.playList');
     const playListButton = document.querySelector('.playlist-icon');
-    
+    const list = await load_playlist();
+
 
     /* PLAYLIST EVENT */
     playListButton.addEventListener('click', () => {
-        playList.classList.toggle('display');
-        console.log('Hola');
+        playListContainer.classList.toggle('display');
     });
 
+    loadPlayList(playList, list);
+    
+
+
 });
+
+
+async function load_playlist(){
+    const list = await fetch('src/assets/playList/playlist.json').then(e => e.json());
+    return list;
+}
+
+
+function loadPlayList(list, data){
+    let count = 1;
+    data.forEach(element => {
+        const p = document.createElement('p');
+        p.setAttribute('class','list-item');
+        p.textContent = `${count}. ${element.artist} - ${element.title}`;
+        list.appendChild(p);
+        count++;
+    });
+}
 
 
 
