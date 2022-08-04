cc051227d6f51845e2f601899823bffe7b723917
Luis Salcedo <luis.salcedo0906@gmail.com>
2022-08-03 20:49:03 -0500

    functions for song selection and playback

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,18 @@
+let list = '';
+const audio = document.querySelector('audio');
+const jsmediatags = window.jsmediatags;
+const previousButton = document.querySelector('#previous');
+const playButton = document.querySelector('#play');
+const nextButton = document.querySelector('#next');
+const playListContainer = document.querySelector('.play-list-container');
+const playList = document.querySelector('.playList');
+const playListButton = document.querySelector('.playlist-icon');
+let song = 0;
+
+
 document.addEventListener('DOMContentLoaded', async function(){
-    const jsmediatags = window.jsmediatags;
-    const playListContainer = document.querySelector('.play-list-container');
-    const playList = document.querySelector('.playList');
-    const playListButton = document.querySelector('.playlist-icon');
-    const list = await load_playlist();
+    list = await load_playlist();
+    song = Math.floor(Math.random()*list.length);
 
 
     /* PLAYLIST EVENT */
@@ -11,10 +20,17 @@ document.addEventListener('DOMContentLoaded', async function(){
         playListContainer.classList.toggle('display');
     });
 
-    loadPlayList(playList, list);
+    /*  loading playList */
+    loadPlayList();
     
 
+    /*  PREPARING SONG */
+    audio.setAttribute('src',`src/assets/songs/${list[song].file}`);
 
+
+    /* CONTROL EVENTS */
+    play();
+    
 });
 
 
@@ -24,16 +40,42 @@ async function load_playlist(){
 }
 
 
-function loadPlayList(list, data){
+function loadPlayList(){
     let count = 1;
-    data.forEach(element => {
-        const p = document.createElement('p');
-        p.setAttribute('class','list-item');
-        p.textContent = `${count}. ${element.artist} - ${element.title}`;
-        list.appendChild(p);
+    list.forEach(element => {
+        const li = document.createElement('li');
+        li.setAttribute('class','list-item');
+        li.setAttribute('data-num', count-1);
+        li.textContent = `${count}. ${element.artist} - ${element.title}`;
+        selectSong(li);
+        playList.appendChild(li);
         count++;
     });
 }
 
+
+function play(){
+    let root = 'src/assets/icons/';
+    playButton.addEventListener('click', ()=>{
+        if(audio.paused){
+            audio.play();
+            playButton.setAttribute("src",root+"pause.svg");
+        }
+        else{
+            audio.pause();
+            playButton.setAttribute("src",root+"play_arrow.svg");
+        }
+    });
+
+}
+
+function selectSong(li){
+    li.addEventListener('click', () => {
+        song = li.dataset.num;
+        audio.setAttribute('src',`src/assets/songs/${list[song].file}`);
+        audio.play();
+        playButton.setAttribute("src","src/assets/icons/pause.svg");
+    });
+}
 
 
